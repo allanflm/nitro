2bc7a2e3f78e1c05861a0dff694c235223090894
Allan <allanfelipedk@gmail.com>
2024-02-13 07:07:56 -0300

    aula 01 e aula 02 - estrura da linguagem c++

---
+++ aulas/aula02.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // adiciona a biblioteca de entrada e saida
+
+using namespace std; // scopo,
+
+int main()
+{
+    // imprime hello world
+    cout << "Hello World" << endl;
+
+    return 0;
+}
